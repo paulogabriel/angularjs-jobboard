dbfae163ae8a71144c4dbcfa7aeb235542a25c46
paulogabriel <paulo84@gmail.com>
2016-03-25 19:11:29 -0300

    Working board

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 // App
-var app = angular.module('vagasApp', []);
+var app = angular.module('vagasApp', ['ngRoute']);
 
+// Controllers
+app.controller('HomeController', ['$scope', 'vagas', function($scope, vagas) {
+  vagas.success(function(data) {
+    $scope.vagas = data;
+  });
+}]);
+
+
+app.controller('VagaController', ['$scope', 'vagas', '$routeParams', function($scope, vagas, $routeParams) {
+  vagas.success(function(data) {
+    $scope.detail = data[$routeParams.id];
+  });
+}]);
+
+// Services
+app.service('vagas', ['$http', function($http) {
+	return $http.get('js/list.json')
+	.success(function(data) {
+		return data;
+	})
+	.error(function(data) {
+		return data;
+	});
+}]);
+
+// Routing
 app.config(function ($routeProvider) {
   $routeProvider
   	.when('/', {
+    	controller: 'HomeController',
+    	templateUrl: 'views/home.html'
+  	})
+  	.when('/#', {
+    	controller: 'HomeController',
+    	templateUrl: 'views/home.html'
+  	})
+  	.when('/#/', {
     	controller: 'HomeController',
     	templateUrl: 'views/home.html'
   	})
@@ -15,54 +49,3 @@ app.config(function ($routeProvider) {
   		redirectTo: '/'
   	});
 });
-
-// Controller + Service
-app.controller('HomeController', function($scope,$http) {
-	var url = "http://paulogabriel.me/extranet/labs/angularjs-jobboard/js/list.json"
-    $http.get(url).success(function (response) {
-       $scope.vagas = response;
-    });
-});
-
-app.controller('VagaController', ['$scope', 'vagas', '$routeParams', function($scope, vagas, $routeParams) {
-  vagas.success(function(data) {
-    $scope.detail = data[$routeParams.id];
-  });
-}]);
-
-// Controller
-// app.controller('MainController', ['$scope', 'apps', function($scope, apps) {
-// 	apps.success(function(data) {
-// 		$scope.apps = data;
-// 	});
-// }]);
-
-// Services
-// app.service('apps', ['$http', function($http) {
-
-// 	return $http({method: 'GET', url: 'https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json'}).success(function(data) {
-// 	    $scope.apps = JSON.parse(data);
-// 	    console.log($scope.apps);
-// 	    $scope.message = "it works!";
-// 	});
-//
-	// return $http.get('http://paulogabriel.me/extranet/labs/angularjs-jobboard/js/list.json')
-	// .success(function(data) {
-	// 	return data;
-	// })
-	// .error(function(data) {
-	// 	return data;
-	// });
-//
-// }]);
-
-// Directives
-// app.directive('appInfo', function() {
-// 	return {
-// 		restrict: 'E',
-// 		// scope: {
-// 			//   info: '='
-// 			// },
-// 			templateUrl: 'appInfo.html'
-// 		};
-// 	});
--- saveit.php
+++ saveit.php
@@ -2,13 +2,16 @@
 
 $filetxt = 'js/list.json';
 
+$current_date = gmDate("Y-m-d\TH:i:s\Z"); 
+
 $formdata = array(
 	'job'=> $_POST['job'],
 	'company'=> $_POST['company'],
 	'city'=> $_POST['city'],
 	'category'=> $_POST['category'],
 	'type'=> $_POST['type'],
-	'description'=> $_POST['description']		
+	'description'=> $_POST['description'],
+	'time'=> $current_date
 );
 
 $arr_data = array();  
@@ -16,8 +19,24 @@ $jsondata = file_get_contents($filetxt);
 $arr_data = json_decode($jsondata, true);
 $arr_data[] = $formdata;
 $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-file_put_contents('js/list.json', $jsondata);
 
-header("location:index.html");
-	
+$captcha;
+if(isset($_POST['g-recaptcha-response'])){
+	$captcha=$_POST['g-recaptcha-response'];
+}
+if(!$captcha){
+	echo ('<h2>Please fill the captcha.</h2>');
+	exit;
+}
+$secretKey = "6LdswxsTAAAAAApgUCIALMCOraDyzXQQnTWh-Tsx";
+$ip = $SERVER['REMOTE_ADDR'];
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+$responseKeys = json_decode($response,true);
+if(intval($responseKeys["success"]) !== 1) {
+	echo '<h2>You are spammer!</h2>';
+} else {
+	file_put_contents('js/list.json', $jsondata);
+	header("location:https://paulogabriel.me/extranet/labs/angularjs-jobboard/");
+}
+
 ?>
